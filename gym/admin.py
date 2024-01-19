83749c27d23bbc1cc0bc526dd219ff43bcaa42b8
Susosky <pipeprocampo@gmail.com>
2024-01-19 00:19:35 -0500

    agg modelos en  /admin

--- gym/admin.py
+++ gym/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import  User, Reserva, Penalizacion
+from .models import User, Reserva, Penalizacion, Asistencia, HorarioDia
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -15,4 +15,15 @@ class ReservaAdmin(admin.ModelAdmin):
 @admin.register(Penalizacion)
 class PenalizacionAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'fecha_inicio', 'fecha_fin', 'id_penalizacion')
-    search_fields = ('usuario__codigo_estudiantil','id_penalizacion')
+    search_fields = ('usuario__codigo_estudiantil', 'id_penalizacion')
+
+@admin.register(Asistencia)
+class AsistenciaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'fecha', 'hora', 'cantidad_horas', 'id_asistencia')
+    list_filter = ('usuario', 'fecha')
+    search_fields = ('usuario__codigo_estudiantil', 'fecha')
+
+@admin.register(HorarioDia)
+class HorarioDiaAdmin(admin.ModelAdmin):
+    list_display = ('dia', 'hora_apertura', 'hora_cierre')
+    search_fields = ('dia',)
